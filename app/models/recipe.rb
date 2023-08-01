16aa25494e9397f3f6e5c8bf6adfbfee3757c3fb
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-08-01 15:36:48 -0400

    Modeling User and Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
+
+  validates :name, presence: true
+  validates :description, presence: true
 end
