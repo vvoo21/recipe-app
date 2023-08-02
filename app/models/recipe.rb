3f9b59b40f9fc4c317a9e440a02e1a3c83158e90
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-08-02 18:16:58 -0400

    Modeling recipes and recipe food

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,16 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
+  has_many :foods, through: :recipe_foods
 
   validates :name, presence: true
   validates :description, presence: true
+
+  def calc_price
+    price = 0
+    recipe_foods.includes(:food).each do |recipe_food|
+      price += recipe_food.quantity * recipe_food.food.price
+    end
+    price.round(2)
+  end
 end
--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,7 @@
 class RecipeFood < ApplicationRecord
   belongs_to :recipe
   belongs_to :food
+
+  validates_numericality_of :quantity, only_integer: true, greater_than_or_equal: 0
+  validates_uniqueness_of :food_id, scope: :recipe_id
 end
