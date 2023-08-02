a44fda024eef554b32953fea7ad6650f44caf2a4
vvoo21 <vaneolipa@gmail.com>
2023-08-02 16:42:01 -0500

    Add validation for food Model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,8 @@
 class Food < ApplicationRecord
   has_many :recipe_foods, dependent: :destroy
   has_many :inventory_foods, dependent: :destroy
+
+  validates :name, presence: true, length: { in: 2..50 }
+  validates :measurement_unit, presence: true, length: { in: 2..12 }
+  validates :price, numericality: { only_integer: false, greater_than_or_equal_to: 0 }
 end
