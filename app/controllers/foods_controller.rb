5a59cf24f11fadf426278ebac502a093009eb597
vvoo21 <vaneolipa@gmail.com>
2023-08-02 17:22:36 -0500

    Delete update method for foods controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,10 @@
 class FoodsController < ApplicationController
-  before_action :set_food, only: %i[show edit update destroy]
+  before_action :set_food, only: %i[show destroy]
 
   # GET /foods or /foods.json
   def index
-    @foods = Food.all
+    @foods = Food.includes(:user)
   end
-
-  # GET /foods/1 or /foods/1.json
-  def show; end
 
   # GET /foods/new
   def new
@@ -29,21 +26,9 @@ class FoodsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /foods/1 or /foods/1.json
-  def update
-    respond_to do |format|
-      if @food.update(food_params)
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully updated.' }
-        format.json { render :show, status: :ok, location: @food }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /foods/1 or /foods/1.json
   def destroy
+    @food = Food.find_by(id: params[:id])
     @food.destroy
 
     respond_to do |format|
