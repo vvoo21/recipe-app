5d792b50d1af86991aa4b59a0f092a3126a9ff11
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-08-02 22:31:58 +0500

    Add authorizations as per requirements

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,4 +1,22 @@
 class Ability
   include CanCan::Ability
-  #Add authorizations
+
+  def initialize(user)
+    user ||= User.new # Guest user (not logged in)
+
+    # All users can see details of public recipes
+    can :read, Recipe, public: true
+
+    # Only the owners of the recipes can add a new food to a recipe
+    can :create, Food, recipe: { user_id: user.id }
+
+    # Only the owners of the inventory can delete it
+    can :destroy, Inventory, user_id: user.id
+
+    # Users could see the inventory details
+    can :read, Inventory, user_id: user.id
+
+    # Users can add new food
+    can :create, Food
+  end
 end
