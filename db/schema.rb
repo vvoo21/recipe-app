575e1d82f5dabed7e0b2d43e94b909728366ef7b
vvoo21 <vaneolipa@gmail.com>
2023-08-03 07:21:29 -0500

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_01_214605) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_03_025235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_214605) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
   end
 
   create_table "inventories", force: :cascade do |t|
@@ -32,12 +34,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_214605) do
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "food_id", null: false
+    t.index ["food_id"], name: "index_inventory_foods_on_food_id"
   end
 
   create_table "recipe_foods", force: :cascade do |t|
     t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "food_id", null: false
+    t.index ["food_id"], name: "index_recipe_foods_on_food_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -63,4 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_01_214605) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "foods", "users"
+  add_foreign_key "inventory_foods", "foods"
+  add_foreign_key "recipe_foods", "foods"
 end
