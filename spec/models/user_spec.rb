702508e0478f3ddc708e18fa31953cc6f257bcbe
vvoo21 <vaneolipa@gmail.com>
2023-08-03 16:43:42 -0500

    install gem 'shoulda-matchers'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'vanessa', photo: 'https://pixabay.com/photos/cat-baby-profile-animal-shelter-2671159/', bio: 'A passionate full-stack developer', posts_count: 0) }
+
+  before { subject.save }
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:bio) }
+    it { should validate_length_of(:bio).is_at_least(2) }
+    it { should validate_length_of(:bio).is_at_most(250) }
+    it { should validate_numericality_of(:posts_count).is_greater_than_or_equal_to(0).only_integer }
+  end
+
+  it 'has many recipes' do
+    post1 = subject.posts.new(title: 'Hello Rails', text: 'This is my first post.')
+    post1.save
+    post2 = subject.posts.new(title: 'Goodbye Rails', text: 'This is my last post.')
+    post2.save
+
+    expect(subject.posts).to eq([post1, post2])
+  end
 end
