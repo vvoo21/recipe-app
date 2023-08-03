a725d84dd0f801de245fb93e6afd112db39d45f3
Feliverse <114632980+Feliverse@users.noreply.github.com>
2023-08-03 17:15:10 -0400

    initialized Models specs

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -1,18 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "users/new", type: :view do
+RSpec.describe 'users/new', type: :view do
   before(:each) do
     assign(:user, User.new(
-      name: "MyString"
-    ))
-  end
-
-  it "renders new user form" do
-    render
-
-    assert_select "form[action=?][method=?]", users_path, "post" do
-
-      assert_select "input[name=?]", "user[name]"
-    end
+                    name: 'MyString'
+                  ))
   end
 end
--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,14 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "users/show", type: :view do
+RSpec.describe 'users/show', type: :view do
   before(:each) do
     assign(:user, User.create!(
-      name: "Name"
-    ))
-  end
-
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Name/)
+                    name: 'Name'
+                  ))
   end
 end
