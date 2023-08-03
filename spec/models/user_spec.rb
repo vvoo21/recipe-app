531d45f657ad9d8254d3a737da04493872ada491
vvoo21 <vaneolipa@gmail.com>
2023-08-03 18:38:46 -0500

    Add unit specs to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'vanessa', photo: 'https://pixabay.com/photos/cat-baby-profile-animal-shelter-2671159/', bio: 'A passionate full-stack developer', posts_count: 0) }
+  it { should have_many(:recipes).dependent(:destroy) }
+  it { should have_many(:foods).dependent(:destroy) }
 
-  before { subject.save }
+ 
+  it { should validate_presence_of(:name) }
+  it { should validate_length_of(:name).is_at_least(2).is_at_most(50) }
 
-  describe 'validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:bio) }
-    it { should validate_length_of(:bio).is_at_least(2) }
-    it { should validate_length_of(:bio).is_at_most(250) }
-    it { should validate_numericality_of(:posts_count).is_greater_than_or_equal_to(0).only_integer }
-  end
-
-  it 'has many recipes' do
-    post1 = subject.posts.new(title: 'Hello Rails', text: 'This is my first post.')
-    post1.save
-    post2 = subject.posts.new(title: 'Goodbye Rails', text: 'This is my last post.')
-    post2.save
-
-    expect(subject.posts).to eq([post1, post2])
+ 
+  it "includes Devise modules" do
+    expect(User.devise_modules).to include(:database_authenticatable, :registerable, :recoverable, :rememberable, :validatable)
   end
 end
+
