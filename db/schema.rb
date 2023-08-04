39ab91715883cb1acb6e331e1a77cfa04f97ba12
vvoo21 <vaneolipa@gmail.com>
2023-08-04 13:22:00 -0500

    New schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_04_065415) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_04_173950) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_04_065415) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.bigint "user_id", null: false
+    t.bigint "food_id", null: false
+    t.index ["food_id"], name: "index_inventories_on_food_id"
     t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
@@ -78,6 +80,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_04_065415) do
   end
 
   add_foreign_key "foods", "users"
+  add_foreign_key "inventories", "foods"
   add_foreign_key "inventories", "users"
   add_foreign_key "inventory_foods", "foods"
   add_foreign_key "inventory_foods", "inventories"
