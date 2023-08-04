f9c9366aab00a3cc3b393f5317151f62a3f57b23
vvoo21 <vaneolipa@gmail.com>
2023-08-04 07:50:42 -0500

    Add tests for Users controller

--- spec/views/users/new.html.erb_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe 'users/new', type: :view do
-  before(:each) do
-    assign(:user, User.new(
-                    name: 'MyString'
-                  ))
-  end
-end
